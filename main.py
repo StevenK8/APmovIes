0df7189d7271583566f9a52e0ce63436ff6b21e1
STEVEN KERAUTRET <steven928@hotmail.fr>
2022-03-15 17:15:11 +0100

    add rating range

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def parse_title_tmdb(title):
 async def post_comment(
         apikey: str,
         movieName: str,
-        rate: float,
+        rate: float = Query(..., gt=0, lt=10),
         comment: Optional[str] = Query(None, max_length=150)):
 
     movieName = parse_title(movieName)
