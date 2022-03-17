577ff6345897a57dff922365f725f396ccc17dea
STEVEN KERAUTRET <steven928@hotmail.fr>
2022-03-17 14:03:01 +0100

    add test get comment

--- test.py
+++ test.py
@@ -9,32 +9,44 @@ CONFIG.read(CONFIG_PATH)
 
 
 async def test_movie_rating():
-    avatar = await main.get_movie_rating_api("Avatar")
+    avatar = main.get_movie_rating_api("Avatar")
     assert 0 <= avatar["rating"] <= float('inf')
     assert 0 <= avatar["vote_count"] <= float('inf')
     assert isinstance(avatar["original_title"], str)
 
 async def test_movie_rating_imdb():
-    avatar = await main.get_movie_rating_imdb("Avatar")
+    avatar = main.get_movie_rating_imdb("Avatar")
     assert 0 <= avatar["rating"] <= 10
     assert 0 <= avatar["vote_count"] <= float('inf')
     assert isinstance(avatar["original_title"], str)
 
 async def test_movie_rating_tmdb():
-    avatar = await main.get_movie_rating_tmdb("Avatar")
+    avatar = main.get_movie_rating_tmdb("Avatar")
     assert 0 <= avatar["rating"] <= 10
     assert 0 <= avatar["vote_count"] <= float('inf')
     assert isinstance(avatar["original_title"], str)
 
 async def test_movie_rating_metacritic():
-    avatar = await main.get_movie_rating_metacritic("Avatar")
+    avatar = main.get_movie_rating_metacritic("Avatar")
     assert 0 <= avatar["rating"] <= 10
     assert isinstance(avatar["original_title"], str)
 
 async def test_post_comment():
-    postResult = await main.post_comment("4a5fb1e7-9002-11ec-92c2-7c0507cfc855", "Avatar", "commentaire", 10)
+    postResult = await main.post_comment("4a5fb1e7-9002-11ec-92c2-7c0507cfc855", "Avatar", 10, "commentaire")
     assert isinstance(postResult["comment"],str)
     assert 0 <= postResult["rate"] <= float('inf') 
+    
+async def test_get_comments():
+    comments = main.get_comments("4a5fb1e7-9002-11ec-92c2-7c0507cfc855")
+    assert isinstance(comments, list)
+    assert isinstance(comments[0]["comment"],str)
+    assert 0 <= comments[0]["rate"] <= float('inf')
+    
+async def test_get_comments_movie():
+    comments = main.get_comments_movie("Avatar")
+    assert isinstance(comments, list)
+    assert isinstance(comments[0]["comment"],str)
+    assert 0 <= comments[0]["rate"] <= float('inf')
 
 
     
